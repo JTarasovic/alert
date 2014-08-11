e5936f1397d8eecad68bef55da62ff680bcc6d5f
Jason Tarasovic <jason@tarasovic.com>
2014-08-11 16:41:09 -0500

    streaming working

--- alert.js
+++ alert.js
@@ -1,21 +1,71 @@
 #!/usr/bin/env node
 var Pushbullet = require('pushbullet'),
-    pusher = new Pushbullet(process.env.ALERT_PUSH_API),
-    exec = require('child_process').exec,
-    text = process.argv.slice(2);
+    stream = new Pushbullet(process.env.ALERT_PUSH_API).stream();
+    // exec = require('child_process').exec,
+    // text = process.argv.slice(2);
 
-var child = exec("notify-send --urgency=low -i " + text.join(' '),
-  function (err, stdout, stderr) {
-    if (err) {
-      console.log('exec error: ' + err);
-    }
-});
+// var child = exec("notify-send --urgency=low -i " + text.join(' '),
+//   function (err, stdout, stderr) {
+//     if (err) {
+//       console.log('exec error: ' + err);
+//     }
+// });
+//
+//
+// pusher.note('',text[1],text[1], function(err, resp){
+//     if(err){
+//         console.log(err);
+//         return;
+//     }
+//     console.log(resp);
+// })
 
 
-pusher.note('',text[1],text[1], function(err, resp){
-    if(err){
-        console.log(err);
-        return;
-    }
-    console.log(resp);
-})
+var connect = function() {
+    console.log('Connected!');
+}
+
+var close = function() {
+    console.log('Connection closed');
+    process.exit();
+}
+
+var nop = function() {
+    console.log('Keep-alive nop received');
+}
+
+var tickle = function(type) {
+    console.dir(type);
+}
+
+var push = function(push) {
+    console.log(push);
+}
+
+var error = function(err) {
+    console.error(err);
+    stream.close();
+}
+
+var exit = function() {
+    console.log('Received interrupt. Exiting.')
+    stream.close();
+}
+
+var msg = function(msg) {
+    console.log(msg);
+}
+
+stream.on('connect', connect);
+stream.on('close', close);
+//stream.on('nop', nop);
+//stream.on('tickle', tickle);
+stream.on('push', push);
+stream.on('error', error);
+//stream.on('message', msg);
+
+process.on('SIGHUP', exit);
+process.on('SIGTERM', exit);
+process.on('SIGINT', exit);
+
+stream.connect();
