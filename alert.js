038fee9b1387ac7d7672ba75bbf489ddaa1b81b6
Jason Tarasovic <jason@tarasovic.com>
2014-08-11 17:30:14 -0500

    clean up old code. go back through logs to find relevent portions, if needed

--- alert.js
+++ alert.js
@@ -12,25 +12,6 @@ var Pushbullet = require('pushbullet'),
     // so that we know when the "last time" we checked was
     time = new Date().getTime() / 1000;
 
-    // exec = require('child_process').exec,
-    // text = process.argv.slice(2);
-
-// var child = exec("notify-send --urgency=low -i " + text.join(' '),
-//   function (err, stdout, stderr) {
-//     if (err) {
-//       console.log('exec error: ' + err);
-//     }
-// });
-//
-//
-// pusher.note('',text[1],text[1], function(err, resp){
-//     if(err){
-//         console.log(err);
-//         return;
-//     }
-//     console.log(resp);
-// })
-
 
 // this will eventually do the actual alerting.
 // it gets the response from the fetched updates and creates alert
@@ -118,28 +99,11 @@ var exit = function() {
     stream.close();
 }
 
-// var nop = function() {
-//     console.log('Keep-alive nop received');
-// }
-//
-// var push = function(push) {
-//     console.log(push);
-// }
-//
-// var msg = function(msg) {
-//     console.log(msg);
-// }
-
-
 // event handlers
 stream.on('connect', connect);
 stream.on('close', close);
 stream.on('tickle', tickle);
 stream.on('error', error);
-//stream.on('nop', nop);
-//stream.on('push', push);
-//stream.on('message', msg);
-
 
 // catch SIGNALS so we can gracefully exit
 process.on('SIGHUP', exit);
